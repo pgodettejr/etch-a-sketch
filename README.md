740d8e0e2d34f1e6c336fba0595fadcd5a5b5e47
pgodettejr <paul.godette@gmail.com>
2023-02-21 14:37:04 -0500

    Create & modify Etch-a-Sketch project
    Created files for the Etch-a-Sketch UI and code to live in. Included
    pseudocoding in JS file for next steps

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # etch-a-sketch
+This is a JavaScript project to build a browser version of something between a sketchpad and Etch-A-Sketch. This project will 
+showcase DOM manipulation skills in JavaScript as well as some CSS.
+
---
+++ etch.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('#container');
+
+// Need one giant grid square split into 4 squares evenly
+// Then those squares split into 4 squares evenly
+// Then those squares split the same way to make a 16 x 16 grid
+
+// Do we need to make every single div 16 times?
+/* Is there a way to shorten the code using math & a function to 
+create the grid for us?*/
+
+/* Worst case: one div with four divs inside it & each of those divs
+have four divs inside those until we get to 16 x 16 */
+
+// Learn CSS grid?
